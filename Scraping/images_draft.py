ea0bf6f7bc6b193f8f6280b42b0a7db93db3eaf7
leonardoviotti <leonardoviotti@gmail.com>
2019-07-26 15:43:32 -0400

    Small advances in Scrapping/images_draft.py

--- Scraping/images_draft.py
+++ Scraping/images_draft.py
@@ -5,25 +5,38 @@
 
 import re
 import requests
+import time
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
 site = 'https://www.wikiaves.com/midias.php?t=s&s=10451'
 
-response = requests.get(site)
 
-soup = BeautifulSoup(response.text, 'html.parser')
-img_tags = soup.find_all('img')
+driver = webdriver.Chrome()
+driver.get(site)
 
-urls = [img['src'] for img in img_tags]
+time.sleep(5)
 
 
-for url in urls:
-    filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
-    with open(filename.group(1), 'wb') as f:
-        if 'http' not in url:
-            # sometimes an image source can be relative 
-            # if it is provide the base url which also happens 
-            # to be the site variable atm. 
-            url = '{}{}'.format(site, url)
-        response = requests.get(url)
-        f.write(response.content)
+foo = driver.find_element_by_class_name('img-responsive')
+foo = driver.find_element_id('img')
+
+
+# response = requests.get(site.format(point_num))
+
+# soup = BeautifulSoup(response.text, 'html.parser')
+# img_tags = soup.find_all('img')
+
+# urls = [img['src'] for img in img_tags]
+
+
+# for url in urls:
+#     filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+#     with open(filename.group(1), 'wb') as f:
+#         if 'http' not in url:
+#             # sometimes an image source can be relative 
+#             # if it is provide the base url which also happens 
+#             # to be the site variable atm. 
+#             url = '{}{}'.format(site, url)
+#         response = requests.get(url)
+#         f.write(response.content)
