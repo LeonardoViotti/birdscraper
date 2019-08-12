0b2781d1d1b77f57c9a181cfa573e344bdebbd3b
LeonardoViotti <leonardoviotti@gmail.com>
2019-08-12 17:16:41 -0400

    Added draft code to download images in Scraping/images_draft.py

--- Scraping/images_draft.py
+++ Scraping/images_draft.py
@@ -6,10 +6,17 @@
 import re
 import requests
 import time
+import shutil
 from selenium import webdriver
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 
 site = 'https://www.wikiaves.com/midias.php?t=s&s=10451'
+
+#--------------------------------------------------------------------
+# Directories
+
+IMAGES_folder = '/home/lviotti/Dropbox/Work/Pessoal/Pokedex'
+
 
 
 #--------------------------------------------------------------------
@@ -22,9 +29,7 @@ driver.get(site)
 # Wait for the website to load
 time.sleep(5)
 
-# Scroll down all the way
-#driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
+#### Scroll down all the way. Since it loads images only when scrolling
 
 # Get scroll height
 last_height = driver.execute_script("return document.body.scrollHeight")
@@ -34,7 +39,7 @@ while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     # Wait to load page
-    time.sleep(3)
+    time.sleep(2)
 
     # Calculate new scroll height and compare with last scroll height
     new_height = driver.execute_script("return document.body.scrollHeight")
@@ -51,8 +56,38 @@ while True:
 images = driver.find_elements_by_tag_name('img')
 
 
-for image in images:
-    print(image.get_attribute('src'))
+# # Construct list with all the links
+# image_links = []
+# for image in images:
+#     print(image.get_attribute('src'))
+
+
+# img1 = 'https://s3.amazonaws.com/media.wikiaves.com.br/images/023/320805q_7e0d5d33e421b7bea6a2987f86ef8f29.jpg'
+# img2 = 'https://s3.amazonaws.com/media.wikiaves.com.br/images/193/391632q_313cd15e81a15cd775c62b137884d4df.jpg'
+
+# foo = [img1, img2]
+
+
+#### Define downloading functions
+
+# Save to file function
+def save_image_to_file(image, dirname, suffix):
+    with open('{dirname}/img_{suffix}.jpg'.format(dirname=dirname, suffix=suffix), 'wb') as out_file:
+        shutil.copyfileobj(image.raw, out_file)
+
+# Download from link function
+def download_images(dirname, links):
+    length = len(links)
+    for index, link in enumerate(links):
+        print 'Downloading {0} of {1} images'.format(index + 1, length)
+        url = link
+        response = requests.get(url, stream=True)
+        save_image_to_file(response, dirname, index)
+        del response
+
+
+download_images('/home/lviotti/Desktop', foo)
+
 
 # response = requests.get(site.format(point_num))
 
