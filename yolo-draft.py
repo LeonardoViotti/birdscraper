1ef467b06c52964c296e9535e04ce177756f8f83
LeonardoViotti <leonardoviotti@gmail.com>
2020-06-03 18:23:00 -0400

    Trying a simpler approuch, but still not working.

--- yolo-draft.py
+++ yolo-draft.py
@@ -8,7 +8,8 @@
 # Settings
 
 import cv2
-from darknet import Darknet
+import cvlib as cv
+#from darknet import Darknet
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,22 +19,32 @@ import matplotlib.pyplot as plt
 
 DATA = 'C:/Users/wb519128/Dropbox/Work/Pessoal/Pokedex/'
 CODE = 'C:/Users/wb519128/GitHub/pokedex/'
-YOLO = CODE + 'yolo-coco/'
+# YOLO = CODE + 'yolo-coco/'
 
-#--------------------------------------------------------------------
-# Yolo settings
-yolo_cfg_file = YOLO + 'yolov3.cfg'
-yolo_weight_file = YOLO + 'yolov3.weights'
-yolo_namesfile = YOLO + 'coco.names'
+# #--------------------------------------------------------------------
+# # Yolo settings
+# yolo_cfg_file = YOLO + 'yolov3.cfg'
+# yolo_weight_file = YOLO + 'yolov3.weights'
+# yolo_namesfile = YOLO + 'coco.names'
 
-# Load the network architecture
-m = Darknet(yolo_cfg_file)
+# # Load the network architecture
+# m = Darknet(yolo_cfg_file)
 
-# Load the pre-trained weights
-m.load_weights(yolo_weight_file)
+# # Load the pre-trained weights
+# m.load_weights(yolo_weight_file)
 
-# Load the COCO object classes
-class_names = load_class_names(yolo_namesfile)
+# # Load the COCO object classes
+# class_names = load_class_names(yolo_namesfile)
 
 #--------------------------------------------------------------------
 # Load test images
+im = cv2.imread(DATA + 'Sabia-cica/Pokedeximg_41.jpg')
+
+bbox, label, conf = cv.detect_common_objects(im)
+output_image = draw_bbox(im, bbox, label, conf)
+plt.imshow(output_image)
+plt.show()
+
+cv2.imshow('image',im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
