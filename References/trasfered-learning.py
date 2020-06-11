45be688985b6e1d04ad6b054f8d69cf20d570f6b
LeonardoViotti <leonardoviotti@gmail.com>
2020-06-11 12:51:51 -0400

    Commented transfered-learning to understand it.

--- References/trasfered-learning.py
+++ References/trasfered-learning.py
@@ -5,9 +5,8 @@
 
 # coding: utf-8
 
-# In[1]:
 
-
+# Import  dependencies
 import pandas as pd
 import numpy as np
 import os
@@ -22,59 +21,65 @@ from keras.models import Model
 from keras.optimizers import Adam
 
 
-# In[2]:
 
+# Imports the mobilenet model and discards the last 1000 neuron layer.
+base_model=MobileNet(weights='imagenet',
+                     include_top=False) 
 
-base_model=MobileNet(weights='imagenet',include_top=False) #imports the mobilenet model and discards the last 1000 neuron layer.
-
+# Some processing here that I don't fully understand yet.
 x=base_model.output
 x=GlobalAveragePooling2D()(x)
-x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
-x=Dense(1024,activation='relu')(x) #dense layer 2
-x=Dense(512,activation='relu')(x) #dense layer 3
-preds=Dense(3,activation='softmax')(x) #final layer with softmax activation
+
+# Add dense layers for the model can learn more complex functions 
+# and classify for better results.
+x=Dense(1024,activation='relu')(x) 
+#dense layer 2
+x=Dense(1024,activation='relu')(x)
+#dense layer 3 
+x=Dense(512,activation='relu')(x)
+ #final layer with softmax activation
+preds=Dense(3,activation='softmax')(x)
 
 
-# In[3]:
-
-
+# Create an instance of the Model class based on the original 
+# architecture
 model=Model(inputs=base_model.input,outputs=preds)
 #specify the inputs
 #specify the outputs
-#now a model has been created based on our architecture
 
-
-# In[4]:
-
-
+# Make sure just the added layers are trainable and it is uysing
+# the MobileNet weights as they are.
+# Carinha de gambiarra esse loop mas ta bom
 for layer in model.layers[:20]:
     layer.trainable=False
 for layer in model.layers[20:]:
     layer.trainable=True
 
 
-# In[5]:
+# Pre-process images to be inputed in the network
+train_datagen=ImageDataGenerator(
+    preprocessing_function=preprocess_input) 
+
+train_generator=train_datagen\
+    .flow_from_directory('./train/', # this is where you specify the path to the main data folder
+                         target_size=(224,224),
+                         color_mode='rgb',
+                         batch_size=32,
+                         class_mode='categorical',
+                         shuffle=True)
 
 
-train_datagen=ImageDataGenerator(preprocessing_function=preprocess_input) #included in our dependencies
 
-train_generator=train_datagen.flow_from_directory('./train/', # this is where you specify the path to the main data folder
-                                                 target_size=(224,224),
-                                                 color_mode='rgb',
-                                                 batch_size=32,
-                                                 class_mode='categorical',
-                                                 shuffle=True)
-
-
-# In[33]:
-
-
-model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
+# Compile model
+model.compile(optimizer='Adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 # Adam optimizer
 # loss function will be categorical cross entropy
 # evaluation metric will be accuracy
 
+# Train the model in our set of images
 step_size_train=train_generator.n//train_generator.batch_size
 model.fit_generator(generator=train_generator,
-                   steps_per_epoch=step_size_train,
-                   epochs=5)
+                    steps_per_epoch=step_size_train,
+                    epochs=5)
