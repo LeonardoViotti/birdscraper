a32e5280955ac52db9903ce885a478c0d8a96b92
LeonardoViotti <leonardoviotti@gmail.com>
2021-04-30 11:22:54 -0400

    Minor changes to http request.

--- Scraping/http-draft.py
+++ Scraping/http-draft.py
@@ -1,18 +1,30 @@
 import requests
 import json
-import os
+# import os
 
+# TODO 
+# Remover # da url da foto
+# Baixar foto
+# Construir base de dados das fotos
+# Como adicionar aleatoriedade
+# Fazer backups caso de perda ce conexao e continuar de onde parou. Como?
+# Como saber o numero de paginas?
+
+
+# Open base request string
 with open('get_request.txt', 'r') as file:
     request_str = file.read()
 
-res = requests.get(request_str,  verify=False)
+# Format string to specify species and page
+request_str_ij = request_str.format(code = '10001', page = '15')
+res_ij = requests.get(request_str_ij,  verify=False)
 
-res.text
-json_data = json.loads(res.text)
 
-# Print pretty JSON
-json_formatted_str = json.dumps(json_data, indent=2)
-print(json_formatted_str)
+def process_request(request):
+    json_data = json.loads(request.text)
+    # Pretty JSON
+    json_formatted_str = json.dumps(json_data, indent=2)
+    print(json_formatted_str)
 
-# session = requests.Session()
-# session.get(request_str,  verify=False)
+process_request(res_ij)
+
