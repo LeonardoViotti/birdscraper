240d5984ea81f3cfa7abbb8cebb59c834aa62608
LeonardoViotti <leonardoviotti@gmail.com>
2021-04-30 14:36:55 -0400

    Progress on http request.

--- Scraping/http-draft.py
+++ Scraping/http-draft.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pandas as pd
 # import os
 
 # TODO 
@@ -16,15 +17,26 @@ with open('get_request.txt', 'r') as file:
     request_str = file.read()
 
 # Format string to specify species and page
-request_str_ij = request_str.format(code = '10001', page = '15')
+request_str_ij = request_str.format(code = '10001', page = '4')
 res_ij = requests.get(request_str_ij,  verify=False)
 
 
 def process_request(request):
     json_data = json.loads(request.text)
-    # Pretty JSON
+    # Pretty JSON for printiting
     json_formatted_str = json.dumps(json_data, indent=2)
-    print(json_formatted_str)
+    # print(json_formatted_str)
+    
+    # Convert to df
+    df = pd.DataFrame.from_dict(json_data['registros']['itens']).T
+    # Keep fewer columns
+    df = df[['id', 'local', 'idMunicipio', 'coms', 'likes', 'vis', 'grande', 'link']]
+    # Process links to remove character 
+    df['link'] = df['link'].str.replace('#','')[10]
+    
+    # Num fotos
+    int(json_data['registros']['total'])
+    
+    return df
 
 process_request(res_ij)
-
