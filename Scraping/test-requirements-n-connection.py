b0d455e8d1e203cd33c419cdf83b1eb9c4ced199
LeonardoViotti <leonardoviotti@gmail.com>
2021-05-20 15:22:53 -0400

    Minor changes.

--- Scraping/test-requirements-n-connection.py
+++ Scraping/test-requirements-n-connection.py
@@ -11,15 +11,16 @@ import shutil
 import random
 from time import sleep
 
-from lxml.html import fromstring
+import bs4
 
 
 print('Checking connection..')
 try:
     r = requests.get('https://en.wikipedia.org/')
-    tree = fromstring(r.content)
-    title = tree.findtext('.//title')
-    print('Accessed: {}'.format(title))
+    # tree = fromstring(r.content)
+    # title = tree.findtext('.//title')
+    html = bs4.BeautifulSoup(r.text)
+    print('Accessed: {}'.format(html.title))
 except:
   print("No connection! :/")
 else:
