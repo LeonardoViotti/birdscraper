9b52c1fb039ff4aac469badbe5f63e031e6a20a8
LeonardoViotti <leonardoviotti@gmail.com>
2021-05-26 12:20:22 -0400

    Changes to draft code.

--- Scraping/draft.py
+++ Scraping/draft.py
@@ -2,13 +2,13 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("data-path", type=str)
+    parser.add_argument("data_path", type=str, default="./configs/deep_sort.yaml")
     # parser.add_argument("--path", type=str)
-    parser.add_argument('--codes', nargs='+', type=int)
+    parser.add_argument('--codes', nargs='+', type=int, required=True)
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
-    print(args.path)
-    for i in args.codes:
-        print(i)
+    print(args.data_path)
+    # for i in args.codes:
+    #     print(i)
