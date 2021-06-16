aa279bab69c60c88375a25cd4af9d4ed11262bf7
LeonardoViotti <leonardoviotti@gmail.com>
2021-06-16 10:48:11 -0400

    Change http number of retries.

--- Scraping/bird_crawler.py
+++ Scraping/bird_crawler.py
@@ -84,7 +84,7 @@ class BirdCrawler():
         
         # Connection settings
         session = requests.Session()
-        retry = Retry(connect=3, backoff_factor=0.5)
+        retry = Retry(connect=5, backoff_factor=0.5)
         adapter = HTTPAdapter(max_retries=retry)
         session.mount('http://', adapter)
         session.mount('https://', adapter)
