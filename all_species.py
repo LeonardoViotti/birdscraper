fe4fbf530a727f9498ae4bcf0d353128be55a185
LeonardoViotti <leonardoviotti@gmail.com>
2023-07-18 09:12:49 -0500

    Minor changes to reference sheet download.

--- all_species.py
+++ all_species.py
@@ -14,73 +14,95 @@ import pandas as pd
 import numpy as np
 import requests as req
 from bs4 import BeautifulSoup
+import os
+
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=str, default="./data",
+                        help = 'Path where the downloaded data should be stored.')
+    parser.add_argument("--url-path", dest = 'url', type=str, default="all_species_table_url.txt",
+                        help = 'File in data path containing reference sheet URL.')
+    parser.add_argument("--dry-run", dest="dry_run", action="store_true", default=False, help = "Don't export outputs.")
+    
+    return parser.parse_args()
 
 
-#--------------------------------------------------------------------
-# Get page with all the species
 
-with open('../data/all_species_table_url.txt', 'r') as file:
-    url = file.read()
+if __name__ == "__main__":
+    args = parse_args()
+    
+    url_path = os.path.join(args.path, args.url)
+    
+    #--------------------------------------------------------------------
+    # Get page with all the species
+    with open(url_path, 'r') as file:
+        url = file.read()
+    
+    print(url)
+    
+    res = req.get(url)
 
-res = req.get(url)
+    # Get table element and structure site info
+    table = BeautifulSoup(res.text, 'html.parser').find('table')
+    headings = [th.get_text() for th in table.find("tr").find_all("th")]
 
-# Get table element and structure site info
-table = BeautifulSoup(res.text, 'html.parser').find('table')
-headings = [th.get_text() for th in table.find("tr").find_all("th")]
+    #--------------------------------------------------------------------
+    # Format and make pandas df
 
-#--------------------------------------------------------------------
-# Format and make pandas df
+    # The table is all defined in js code. This loops trough the page a creates a dataframe from it
 
-# The table is all defined in js code. This loops trough the page a creates a dataframe from it
+    # Clean strings functions
+    def str_line_rm(strg, element):
+        return(strg.replace(element, ""))
 
-# Clean strings functions
-def str_line_rm(strg, element):
-    return(strg.replace(element, ""))
+    def str_line_clean(strg, element1, element2):
+        new_str = str_line_rm(strg, element1)
+        new_str = str_line_rm(new_str, element2)
+        return new_str
 
-def str_line_clean(strg, element1, element2):
-    new_str = str_line_rm(strg, element1)
-    new_str = str_line_rm(new_str, element2)
-    return new_str
+    # Loop through all the lines that contain a script tag and process them
+    dt1 = []
+    for row in table.find_all('script')[1:]:
+        row = str(row)
+        row = str_line_clean(row, "<script> lsp(", "); </script>")
+        row = str_line_rm(row, "'")
+        row = row.split(",")
+        dt1.append(row)
 
-# Loop through all the lines that contain a script tag and process them
-dt1 = []
-for row in table.find_all('script')[1:]:
-    row = str(row)
-    row = str_line_clean(row, "<script> lsp(", "); </script>")
-    row = str_line_rm(row, "'")
-    row = row.split(",")
-    dt1.append(row)
+    # Convert them to pandas data.frame
+    data = pd.DataFrame.from_records(dt1)
+    data.columns = ['code', 'family', 'species', 'name_uft8', 'name', 'pic', 'sound']
 
-# Convert them to pandas data.frame
-data = pd.DataFrame.from_records(dt1)
-data.columns = ['code', 'family', 'species', 'name_uft8', 'name', 'pic', 'sound']
+    # Data types
+    data['code'] = data['code'].astype('int')
+    data['family'] = data['family'].astype('str').str.strip()
+    data['species'] = data['species'].astype('str').str.strip()
+    data['name_uft8'] = data['name_uft8'].astype('str').str.strip()
+    data['name'] = data['name'].astype('str').str.strip()
+    data['sound'] = data['sound'].astype('int')
+    data['pic'] = data['pic'].astype('int')
 
-# Data types
-data['code'] = data['code'].astype('int')
-data['family'] = data['family'].astype('str').str.strip()
-data['species'] = data['species'].astype('str').str.strip()
-data['name_uft8'] = data['name_uft8'].astype('str').str.strip()
-data['name'] = data['name'].astype('str').str.strip()
-data['sound'] = data['sound'].astype('int')
-data['pic'] = data['pic'].astype('int')
+    # Fill missing family values
+    family_index = data.index[data['family'] != ''].tolist()
+    missing_index = data.index[data['family'] == ''].tolist()
 
-# Fill missing family values
-family_index = data.index[data['family'] != ''].tolist()
-missing_index = data.index[data['family'] == ''].tolist()
+    # VERY shity way of doing this
+    def smaller_index(list, x):
+        for i in reversed(list):
+            #if i >x: return l.index(i)
+            if i < x: return i
 
-# VERY shity way of doing this
-def smaller_index(list, x):
-    for i in reversed(list):
-        #if i >x: return l.index(i)
-        if i < x: return i
+    # VERY shity way of doing this 2
+    # looping throuh all the lines to replace with the family before
 
-# VERY shity way of doing this 2
-# looping throuh all the lines to replace with the family before
+    for i in missing_index:
+        data['family'][i] = data['family'][smaller_index(family_index, i)]
 
-for i in missing_index:
-    data['family'][i] = data['family'][smaller_index(family_index, i)]
-
-#--------------------------------------------------------------------
-# Export
-if EXPORT_data:
-    data.to_csv("../data/all_species.csv", encoding='utf-8', index=False)
+    #--------------------------------------------------------------------
+    # Export
+    
+    if not args.dry_run:
+        filename = 'all_species.csv'
+        data.to_csv(os.path.join(args.path, filename), encoding='utf-8', index=False)
